fe4cbe19ddf9b1357c5fe4ada9e4cb6904bc77ff
“ELOBO5” <elicialobo@hotmail.com>
2021-08-10 14:37:04 +0100

    add routes

--- server/app.js
+++ server/app.js
@@ -1,39 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const postRoutes = require('./controllers/routes');
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const postRoutes = require("./controllers/routes");
+app.use('/posts', postRoutes);
 
-const fs = require("fs");
-
-let user;
-
-fs.readFile("data.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("Error reading file from disk:", err);
-    return;
-  }
-  try {
-    user = JSON.parse(data);
-    console.log(user);
-  } catch (err) {
-    console.log("Error parsing JSON string:", err);
-  }
+app.get('/', (req, res) => {
+    res.send('Welcome to Love Island Reacts!');
 });
 
-app.get("/posts", (req, res) => {
-  res.send(user);
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Love Island Reacts!");
-});
-
-app.post("/", (req, res) => {
-  res.status(405).send("Not allowed!");
+app.post('/', (req, res) => {
+    res.status(405).send('Not allowed!');
 });
 
 module.exports = app;
