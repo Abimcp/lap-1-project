0c0ce81ecf187e58f448bf6a69a1e93ffbee741a
Tobi Tselios <tobi.tselios@gmail.com>
2021-08-10 12:30:12 +0100

    Helper functions to work with JSON file

---
+++ server/controllers/helpers.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+const writeToJson = data => {
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), error => {
+        if (error) {
+            console.log('Error saving post data to JSON file', error);
+        }
+        console.log('JSON data saved successfully');
+    });
+};
+
+const addNewPost = newPost => {
+    fs.readFile('data.json', 'utf-8', (error, data) => {
+        if (error) {
+            console.log('Error reading data from "data.json"', error);
+        }
+
+        const posts = JSON.parse(data);
+        const updatedPosts = [newPost, ...posts];
+        writeToJson(updatedPosts);
+    });
+};
+
+const addCommentToPost = (postId, comment) => {
+    fs.readFile('data.json', 'utf-8', (error, data) => {
+        if (error) {
+            console.log('Error reading data from "user.json"', error);
+        }
+
+        const posts = JSON.parse(data);
+        const updatedPosts = posts.map(post => {
+            if (post.id === postId) {
+                post.comments.unshift(comment);
+            }
+
+            return posts;
+        });
+
+        writeToJson(...updatedPosts);
+    });
+};
+
+const addReactionToPost = (postId, reaction) => {
+    fs.readFile('data.json', 'utf-8', (error, data) => {
+        if (error) {
+            console.log('Error reading data from "user.json"', error);
+        }
+
+        const posts = JSON.parse(data);
+        const updatedPosts = posts.map(post => {
+            if (post.id === postId) {
+                if (reaction === 'like') {
+                    post.like++;
+                } else if (reaction === 'thumbsUp') {
+                    post.thumbsUp++;
+                } else if (reaction === 'clap') {
+                    post.clap++;
+                }
+            }
+
+            return posts;
+        });
+
+        writeToJson(...updatedPosts);
+    });
+};
+
+module.exports = { addNewPost, addCommentToPost, addReactionToPost };
