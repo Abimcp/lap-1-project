d88a274adb7c65ccdea706954c67e66241d27edc
Tobi Tselios <tobi.tselios@gmail.com>
2021-08-11 11:05:43 +0100

    Disable button when a user hasn't added a post entry

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,7 +1,14 @@
-const { updateCharacterCount, handleGifSearch, fetchPost } = require('./helpers');
+const {
+    updateCharacterCount,
+    handleGifSearch,
+    enablePostButton
+} = require('./helpers');
 
 const postText = document.querySelector('.post__text');
 const searchbar = document.querySelector('.giphy-search__container input');
 
 postText.addEventListener('keyup', updateCharacterCount);
+postText.addEventListener('keydown', enablePostButton);
 searchbar.addEventListener('keydown', handleGifSearch);
+
+enablePostButton();
